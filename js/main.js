083646d6ba47f07982acd39d4bef520501d1a833
Denys <denysbraun1985@gmail.com>
2019-03-04 13:49:17 +0200

    footer and img

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-/*=================PRELOADER======================*/
+/*================================================PRELOADER===========================================================*/
 $(window).on('load', function() {
     $("#preloader").delay(600).fadeOut();
 });
-/*=================preloader======================*/
+/*================================================preloader===========================================================*/
 
-/*=================MAGNIFIC POPUP======================*/
+/*==============================================MAGNIFIC POPUP========================================================*/
 $('.portfolio-card-container').magnificPopup({
     delegate: '.lightbox', // child items selector, by clicking on it popup will open
     type: 'image'
 });
-/*=================magnific popup======================*/
+/*==============================================magnific popup========================================================*/
 
-/*=================EMAIL VALIDATION======================*/
+/*=============================================EMAIL VALIDATION=======================================================*/
 var emailInput;
 
 $("#email").on("change", function() {
@@ -32,4 +32,49 @@ function validateEmail(email) {
 
     return !!$.trim(email).match(pattern);
 }
-/*=================email validation======================*/
+/*=============================================email validation=======================================================*/
+
+
+
+/*=============================================PHONE VALIDATION=======================================================*/
+function clearVal(val, limit){
+    var newVal = val.replace(/[^\d]+/g, '');
+    if( newVal === '' ){
+        return false;
+    }else{
+        return newVal.substring(0, limit);
+    }
+}
+
+function getResString(newVal){
+    var res = '';
+    for(var i = 0; i < newVal.length; i++){
+        if( i === 3 ){
+            res += ' ';
+            res += newVal.charAt(i);
+        }else if( i === 6 || i === 8 ){
+            res += '-';
+            res += newVal.charAt(i);
+        }else{
+            res += newVal.charAt(i);
+        }
+    }
+    return res;
+}
+
+$(function(){
+    $('#phone').on('input', function(){
+        var val = $(this).val(),
+            limit = 10;
+        if( val === '' ) return;
+
+        var newVal = clearVal(val, limit);
+        if(!newVal){
+            $(this).val('');
+            return;
+        }
+        var res = getResString(newVal);
+        $(this).val(res);
+    });
+});
+/*===============================================phone validation=====================================================*/
